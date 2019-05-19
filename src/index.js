8bd117a91de3c22d6d5de53bd0e8fbbfd76b84d5
Luc Patiny <luc@patiny.com>
2019-05-19 21:45:32 +0200

    add ACM to allow serial on fedora

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const slcanEventEmitter = new SlcanEventEmitter();
 
 async function list() {
   let currentAdapters = (await SerialPort.list()).filter(
-    (entry) => entry.comName && entry.comName.match(/usbmodem/i)
+    (entry) => entry.comName && entry.comName.match(/usbmodem|ACM/i)
   );
   for (let currentAdapter of currentAdapters) {
     const serialPort = new SerialPort(currentAdapter.comName);
