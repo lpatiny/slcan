08be5753a904830558482dd33e1130670d71b28e
Sam Sulaimanov <sam@bitmorse.com>
2019-05-22 20:07:04 +0200

    adding raspberry serial port

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const slcanEventEmitter = new SlcanEventEmitter();
 
 async function list() {
   let currentAdapters = (await SerialPort.list()).filter(
-    (entry) => entry.comName && entry.comName.match(/usbmodem|ACM/i)
+    (entry) => entry.comName && entry.comName.match(/usbmodem|ACM|AMA0/i)
   );
   for (let currentAdapter of currentAdapters) {
     const serialPort = new SerialPort(currentAdapter.comName);
