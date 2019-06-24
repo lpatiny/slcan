f692aceeb25e53e334c4acf782d5b5452dd6b896
Luc Patiny <luc@patiny.com>
2019-06-24 05:58:28 +0200

    update to 0.3.1

--- demo/listenFrames.js
+++ demo/listenFrames.js
@@ -1,9 +1,12 @@
 'use strict';
 
+const debug = require('debug')('slcan/listenFrames');
+
 const { updateAdapters } = require('../src');
 
 // we retrieve all the adapters
 updateAdapters().then(async (adapters) => {
+  debug(`Found adapters: ${adapters}`);
   // for each adapter we listen to events
   // the events can be uavcan (a decoded packet) or frame
   for (let adapter of adapters) {
